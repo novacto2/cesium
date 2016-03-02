d02f93a66fa9284d41ae3e704041d2daca1b2329
Matthew Amato <matt.amato@gmail.com>
2016-03-01 22:23:43 -0500

    Ongoing prototyping.

--- gulpfile.js
+++ gulpfile.js
@@ -281,9 +281,8 @@ gulp.task('release', ['combine', 'minifyRelease', 'generateDocumentation']);
 gulp.task('coverage', function(done) {
     karma.start({
                     configFile : karmaConfigFile,
-                    browserDisconnectTolerance : 5,
                     browserNoActivityTimeout : 5 * 60 * 1000,
-                    browserDisconnectTimeout : 5 * 60 * 1000,
+                    browsers: ['Electron'],
                     preprocessors : {
                         'Source/**/*.js' : ['coverage']
                     },
